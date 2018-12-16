48285dde7ce0f4ffaf5c9973d083a86b5f668c80
Jason Nguyen <jaseonwing@gmail.com>
2018-12-15 19:36:16 -0800

    fix ordering and clean up methods

--- Pachinko/Source.c
+++ Pachinko/Source.c
@@ -1,18 +1,46 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
 
+#define BEGINNING_CREDITS (1000)
+#define JACKPOT_MULTIPLIER (3)
 #define LINESIZE (1024)
 #define MINIMUM_CREDITS (100)
-#define BEGINNING_CREDITS (1000)
 #define NUMBER_OF_SYMBOLS (5)
 #define NUMBER_PER_LINE (3)
-#define JACKPOT_MULTIPLIER (3)
 
-int credits;
+size_t credits;
 int playedCredits;
-char symbols[NUMBER_OF_SYMBOLS];
 char spunSymbols[NUMBER_PER_LINE];
+char symbols[NUMBER_OF_SYMBOLS];
 
+/* 
+
+enum eSymbol
+{
+	X,
+	Y,
+	A,
+	B,
+	C
+};
+enum eSymbol symbolsSpun[NUMBER_PER_LINE];
+*/
+
+bool EvaluateResults()
+{
+	if (spunSymbols[0] == spunSymbols[1] && spunSymbols[0] == spunSymbols[2])
+	{
+		credits -= playedCredits;
+		credits += playedCredits * JACKPOT_MULTIPLIER;
+		return true;
+	}
+	else
+	{
+		credits -= playedCredits;
+		return false;
+	}
+}
 
 void Init() 
 {
@@ -25,32 +53,12 @@ void Init()
 	playedCredits = 0;
 }
 
-void Spin()
-{
-	/* Use current time as seed for random generator */
-	srand((unsigned int)time(0));
-	spunSymbols[0] = symbols[rand() % NUMBER_OF_SYMBOLS];
-	spunSymbols[1] = symbols[rand() % NUMBER_OF_SYMBOLS];
-	spunSymbols[2] = symbols[rand() % NUMBER_OF_SYMBOLS];
-}
-
-void UpdateWin()
-{
-	credits -= playedCredits;
-	credits += playedCredits * JACKPOT_MULTIPLIER;
-}
-
-void UpdateLoss()
-{
-	credits -= playedCredits;
-}
-
 void RenderResults() 
 {
 	printf("\nSpinning...\n");
 	printf("%c %c %c\n", spunSymbols[0], spunSymbols[1], spunSymbols[2]);
 
-	if (spunSymbols[0] == spunSymbols[1] && spunSymbols[0] == spunSymbols[2])
+	if (EvaluateResults())
 	{
 		printf("Jackpot! You won %d credits!\n\n", (playedCredits * JACKPOT_MULTIPLIER));
 	}
@@ -64,30 +72,22 @@ void RenderResults()
 	}
 }
 
-void EvaluateResults() 
+void RenderPrompt() 
 {
-	if (spunSymbols[0] == spunSymbols[1] && spunSymbols[0] == spunSymbols[2]) 
-	{
-		UpdateWin();
-	}
-	else
-	{
-		UpdateLoss();
-	}
+	printf("You have %d credits.\n", credits);
+	printf("Input number of credits to play: \n");
 }
 
-void Play()
+void Spin()
 {
-	Spin();
-	EvaluateResults();
+	/* Use current time as seed for random generator */
+	srand((unsigned int)time(0));
+	spunSymbols[0] = symbols[rand() % NUMBER_OF_SYMBOLS];
+	spunSymbols[1] = symbols[rand() % NUMBER_OF_SYMBOLS];
+	spunSymbols[2] = symbols[rand() % NUMBER_OF_SYMBOLS];
 }
 
-void RenderInvalidInput() 
-{
-	fprintf(stderr, "You must enter %d or more credits to play.\n", MINIMUM_CREDITS);
-}
-
-void Update() 
+void Update()
 {
 	char line[LINESIZE];
 	while (1)
@@ -96,27 +96,16 @@ void Update()
 		{
 			clearerr(stdin);
 		}
-		if ((sscanf_s(line, "%d", &playedCredits) == 1) && (playedCredits >= MINIMUM_CREDITS))
+		if ((sscanf_s(line, "%d", &playedCredits) == 1) && (playedCredits >= MINIMUM_CREDITS) && (playedCredits <= credits))
 		{
-			Play();
+			Spin();
 			break;
 		}
 		else
 		{
-			RenderInvalidInput();
+			fprintf(stderr, "You must enter %d or more credits to play.\n", MINIMUM_CREDITS);
 		}
 	}
-}
-
-void RenderPrompt() 
-{
-	printf("You have %d credits.\n", credits);
-	printf("Input number of credits to play: \n");
-}
-
-void Release()
-{
-	getchar();
 }
 
 int main(void)
@@ -128,6 +117,6 @@ int main(void)
 		Update();
 		RenderResults();
 	}
-	Release();
+	getchar();
 	return 0;
 }
